34e4502cc507cf97452a4773b335c8d6d66b9466
Hammarkvast <tomham-3@student.ltu.se>
2019-10-19 17:39:42 +0200

    Restructured

--- tokyoking/KingTokyoPowerUpClient.java
+++ tokyoking/KingTokyoPowerUpClient.java
@@ -1,3 +1,4 @@
+package tokyoking;
 import java.util.*;
 import java.io.*;
 import java.net.*;
--- tokyoking/KingTokyoPowerUpServer.java
+++ tokyoking/KingTokyoPowerUpServer.java
@@ -1,15 +1,21 @@
+package tokyoking;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
+
+import tokyoking.cards.Card;
+import tokyoking.effects.Effect;
+import tokyoking.monsters.Monster;
+import tokyoking.dice.Dice;
+import tokyoking.deck.Deck;
 
 public class KingTokyoPowerUpServer {
 
@@ -23,139 +29,21 @@ public class KingTokyoPowerUpServer {
         new KingTokyoPowerUpServer();
     }
     
-    class Monsters {
-        public int maxHealth = 10;
-        public int currentHealth = 10;
-        public String name;
-        public int energy = 0;
-        public int stars = 0;
-        public boolean inTokyo = false;
-        ArrayList<Card> cards = new ArrayList<Card>();
-        public Socket connection = null;
-        public BufferedReader inFromClient = null;
-        public DataOutputStream outToClient = null;
-       
-        public Monsters(String name) {
-            this.name = name;
-        }
-       
-        // search all available cards and return the effect value of an effect
-        public int cardEffect(String effectName) {
-            for(int i=0; i<cards.size(); i++) {
-                try {
-                    //Find variable by "name"
-                     if(Effect.class.getField(effectName).getInt(cards.get(i).effect) > 0) {
-                         return Effect.class.getField(effectName).getInt(cards.get(i).effect);
-                     }
-                } catch (Exception e) {}
-            }
-            return 0;
-        }
-
-        public String cardsToString() {
-            String returnString = "";
-            if (cards.size()==0)
-                return "[NO CARDS]:";
-            for(int i=0; i<cards.size(); i++) {returnString += "\t["+i+"] " + cards.get(i) + ":";}
-            return returnString;           
-        }
-    }
     
-    class Deck {
-        public ArrayList<Card> deck = new ArrayList<Card>();
-        public Card[] store = new Card[3];
-        
-        public Deck() {
-            Effect moreDamage = new Effect(); moreDamage.moreDamage = 1;
-            Effect cardsCostLess = new Effect(); cardsCostLess.cardsCostLess = 1;
-            Effect starsWhenAttacking = new Effect(); starsWhenAttacking.starsWhenAttacking = 1;
-            Effect stars3 = new Effect(); stars3.stars = 3;
-            Effect armor = new Effect(); armor.armor = 1;
-            Effect stars2 = new Effect(); stars2.stars = 2;
-            Effect stars1 = new Effect(); stars1.stars = 1;
-            deck.add(new Card("Acid Attack", 6, false, moreDamage, "Deal 1 extra damage each turn"));
-            deck.add(new Card("Alien Metabolism", 3, false, cardsCostLess, "Buying cards costs you 1 less"));
-            deck.add(new Card("Alpha Monster", 5, false, starsWhenAttacking, "Gain 1 star when you attack"));
-            deck.add(new Card("Apartment Building", 5, true, stars3, "+3 stars"));
-            deck.add(new Card("Armor Plating", 4, false, armor, "Ignore damage of 1"));
-            deck.add(new Card("Commuter Train", 4, true, stars2, "+2 stars"));
-            deck.add(new Card("Corner Stone", 3, true, stars1, "+1 stars"));
-            //Todo: Add more cards
-            Collections.shuffle(deck);
-            // Start the game with 3 cards face up in the store
-            for(int i=0; i<3; i++) {store[i] = deck.remove(0);}
-        }
-        // Print the store
-        public String toString() {
-            String returnString = "";
-            for(int i=0; i<3; i++) {returnString += "\t["+i+"] " + store[i] + ":";}
-            return returnString;
-        }
-    }
-    
-    class Card {
-        public String name;
-        public int cost;
-        public boolean discard;
-        public Effect effect;
-        public String description;
-        public Card(String name, int cost, boolean discard, Effect effect, String description) {
-            this.name = name;
-            this.cost = cost;
-            this.discard = discard;
-            this.effect = effect;
-            this.description = description;
-        }       
-        public String toString() {
-            return name + ", Cost " + cost + ", " + (discard?"DISCARD":"KEEP") + ", Effect " + description;
-        }
-    }
-    
-    class Effect {
-        public int moreDamage = 0; //Acid Attack
-        public int cardsCostLess = 0; //Alien Metabolism
-        public int starsWhenAttacking = 0; //Alpha monster
-        public int stars = 0; //Apartment Building, Commuter Train, Corner Stone
-        public int armor = 0; //Armor Plating
-    }
-    
-    class Dice implements Comparable<Dice> {
-        public static final int HEART = 0;
-        public static final int ENERGY = 4;
-        public static final int CLAWS = 5;
-        public int value = -1;  
-        public Dice(int value) {
-            this.value = value;
-        }
-        public String toString() {
-            return (value==HEART?"HEART":value==ENERGY?"ENRGY":value==CLAWS?"CLAWS":value==1?"ONE":value==2?"TWO":"THREE");
-        }
-        @Override
-        public int compareTo(Dice o) {
-            return value<o.value?-1:value==o.value?0:1;
-        }
-        public boolean equals(Object o) {
-            return value==((Dice) o).value;
-        }
-        public int hashCode() {
-            return toString().hashCode();
-        }
-    }
-    
-    private ArrayList<Monsters> monsters = new ArrayList<Monsters>();
+    private ArrayList<Monster> monster = new ArrayList<Monster>();
     private Random ran = new Random();
     private Scanner sc = new Scanner(System.in);
 
     
     public KingTokyoPowerUpServer() {
-        Monsters kong = new Monsters("Kong");
-        Monsters gigazaur = new Monsters("Gigazaur");
-        Monsters alien = new Monsters("Alienoid");
-        monsters.add(kong);
-        monsters.add(gigazaur);
-        monsters.add(alien);
+        Monster kong = new Monster("Kong");
+        Monster gigazaur = new Monster("Gigazaur");
+        Monster alien = new Monster("Alienoid");
+        monster.add(kong);
+        monster.add(gigazaur);
+        monster.add(alien);
         //Shuffle which player is which monster
-        Collections.shuffle(monsters);
+        Collections.shuffle(monster);
         Deck deck = new Deck();
        
         //Server stuffs
@@ -166,16 +54,16 @@ public class KingTokyoPowerUpServer {
                 Socket connectionSocket = aSocket.accept();
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
                 DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-                outToClient.writeBytes("You are the monster: " + monsters.get(onlineClient).name + "\n");
-                monsters.get(onlineClient).connection = connectionSocket;
-                monsters.get(onlineClient).inFromClient = inFromClient;
-                monsters.get(onlineClient).outToClient = outToClient;
-                System.out.println("Connected to " + monsters.get(onlineClient).name);
+                outToClient.writeBytes("You are the monster: " + monster.get(onlineClient).name + "\n");
+                monster.get(onlineClient).connection = connectionSocket;
+                monster.get(onlineClient).inFromClient = inFromClient;
+                monster.get(onlineClient).outToClient = outToClient;
+                System.out.println("Connected to " + monster.get(onlineClient).name);
             }
         } catch (Exception e) {}
 
         //Shuffle the starting order
-        Collections.shuffle(monsters);
+        Collections.shuffle(monster);
         /*
             Game loop:
             pre: Award a monster in Tokyo 1 star
@@ -196,8 +84,8 @@ public class KingTokyoPowerUpServer {
             8. Check victory conditions
         */        
         while(true) {
-            for(int i=0; i<monsters.size(); i++) {
-                Monsters currentMonster = monsters.get(i);
+            for(int i=0; i<monster.size(); i++) {
+                Monster currentMonster = monster.get(i);
                 if(currentMonster.currentHealth <= 0) {
                     currentMonster.inTokyo = false;
                     continue;
@@ -206,10 +94,10 @@ public class KingTokyoPowerUpServer {
                 if(currentMonster.inTokyo) {currentMonster.stars += 1;}
                 String statusUpdate = "You are " + currentMonster.name + " and it is your turn. Here are the stats";
                 for(int count=0; count<3; count++) {
-                    statusUpdate += ":"+monsters.get(count).name + (monsters.get(count).inTokyo?" is in Tokyo ":" is not in Tokyo ");
-                    statusUpdate += "with " + monsters.get(count).currentHealth + " health, " + monsters.get(count).stars + " stars, ";
-                    statusUpdate += monsters.get(count).energy + " energy, and owns the following cards:";
-                    statusUpdate += monsters.get(count).cardsToString();
+                    statusUpdate += ":"+monster.get(count).name + (monster.get(count).inTokyo?" is in Tokyo ":" is not in Tokyo ");
+                    statusUpdate += "with " + monster.get(count).currentHealth + " health, " + monster.get(count).stars + " stars, ";
+                    statusUpdate += monster.get(count).energy + " energy, and owns the following cards:";
+                    statusUpdate += monster.get(count).cardsToString();
                 }
                 sendMessage(i, statusUpdate+"\n");
                 // 1. Roll 6 dice
@@ -256,8 +144,8 @@ public class KingTokyoPowerUpServer {
                             //Current support is only for the Red Dawn card
                             //Add support for keeping it secret until played
                             String power = sendMessage(i, "POWERUP:Deal 2 damage to all others\n");
-                            for(int mon=0; mon<monsters.size(); mon++) {
-                                if(mon!=i) {monsters.get(mon).currentHealth+=-2;}
+                            for(int mon=0; mon<monster.size(); mon++) {
+                                if(mon!=i) {monster.get(mon).currentHealth+=-2;}
                             }
                         }
                         if(currentMonster.name.equals("Gigazaur")) {
@@ -292,28 +180,28 @@ public class KingTokyoPowerUpServer {
                 if(result.containsKey(aClaw)) {
                     currentMonster.stars += currentMonster.cardEffect("starsWhenAttacking"); //Alpha Monster
                     if(currentMonster.inTokyo) {
-                        for(int mon=0; mon<monsters.size(); mon++) {
+                        for(int mon=0; mon<monster.size(); mon++) {
                             int moreDamage = currentMonster.cardEffect("moreDamage"); //Acid Attack
                             int totalDamage = result.get(aClaw).intValue()+moreDamage;
-                            if(mon!=i && totalDamage > monsters.get(mon).cardEffect("armor")) { //Armor Plating
-                                monsters.get(mon).currentHealth+=-totalDamage;
+                            if(mon!=i && totalDamage > monster.get(mon).cardEffect("armor")) { //Armor Plating
+                                monster.get(mon).currentHealth+=-totalDamage;
                             }
                         }
                     }
                     else {
                         boolean monsterInTokyo = false;
-                        for(int mon=0; mon<monsters.size(); mon++) {
-                            if(monsters.get(mon).inTokyo){
+                        for(int mon=0; mon<monster.size(); mon++) {
+                            if(monster.get(mon).inTokyo){
                                 monsterInTokyo = true;
                                 int moreDamage = currentMonster.cardEffect("moreDamage"); //Acid Attack
                                 int totalDamage = result.get(aClaw).intValue()+moreDamage;
-                                if(totalDamage > monsters.get(mon).cardEffect("armor")) //Armor Plating
-                                    monsters.get(mon).currentHealth+=-totalDamage;
+                                if(totalDamage > monster.get(mon).cardEffect("armor")) //Armor Plating
+                                    monster.get(mon).currentHealth+=-totalDamage;
                                 // 6e. If you were outside, then the monster inside tokyo may decide to leave Tokyo
                                 String answer = sendMessage(mon, "ATTACKED:You have " + 
-                                    monsters.get(mon).currentHealth + " health left. Do you wish to leave Tokyo? [YES/NO]\n");
+                                    monster.get(mon).currentHealth + " health left. Do you wish to leave Tokyo? [YES/NO]\n");
                                 if(answer.equalsIgnoreCase("YES")) {
-                                    monsters.get(mon).inTokyo = false;
+                                    monster.get(mon).inTokyo = false;
                                     monsterInTokyo = false;
                                 }
                             }
@@ -345,19 +233,19 @@ public class KingTokyoPowerUpServer {
                 }
                 //8. Check victory conditions
                 int alive=0; String aliveMonster = "";
-                for(int mon=0; mon<monsters.size(); mon++) {
-                    if(monsters.get(mon).stars >= 20) {
-                        for(int victory=0; victory<monsters.size(); victory++) {
-                            String victoryByStars = sendMessage(victory, "Victory: " + monsters.get(mon).name + " has won by stars\n");
+                for(int mon=0; mon<monster.size(); mon++) {
+                    if(monster.get(mon).stars >= 20) {
+                        for(int victory=0; victory<monster.size(); victory++) {
+                            String victoryByStars = sendMessage(victory, "Victory: " + monster.get(mon).name + " has won by stars\n");
                         }
                         System.exit(0);
                     }
-                    if(monsters.get(mon).currentHealth > 0) {
-                        alive++; aliveMonster = monsters.get(mon).name;
+                    if(monster.get(mon).currentHealth > 0) {
+                        alive++; aliveMonster = monster.get(mon).name;
                     }
                 }
                 if(alive==1) {
-                    for(int victory=0; victory<monsters.size(); victory++) {
+                    for(int victory=0; victory<monster.size(); victory++) {
                         String victoryByKills = sendMessage(victory, "Victory: " + aliveMonster + " has won by being the only one alive\n");
                     } 
                     System.exit(0);
@@ -367,7 +255,7 @@ public class KingTokyoPowerUpServer {
     }
     
     private String sendMessage(int recipient, String message) {
-        Monsters aMonster = monsters.get(recipient);
+        Monster aMonster = monster.get(recipient);
         String response = "";
         if(aMonster.connection != null) {
             try {
